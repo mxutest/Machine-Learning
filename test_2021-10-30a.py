ad9a0f4e32becba2ce3a00144c1c5478e165c4ff
mxutest <78762808+mxutest@users.noreply.github.com>
2021-10-31 20:31:36 -0400

    Update test_2021-10-30a.py

--- test_2021-10-30a.py
+++ test_2021-10-30a.py
@@ -9,12 +9,22 @@ shows ***stock*** closing and volue of **price**
 """)
 
 
+st.title('Stock Forecast App')
+stocklist = ('AAPL','MSCT')
+option = st.selectbox('Select dataset for prediction',stocklist)
+year = st.slider('Year of prediction:',1,4)
+period = year * 365
 
-tickerSymbol='GOOGL'
-
+tickerSymbol='AAPL'
+data_load_state = st.text('Loading data...')
 tickerData=yf.Ticker(tickerSymbol)
-
-tickerDf=tickerData.history(period='1d',start="2010-5-31",end='2020-5-31')
+data=tickerData.history(period='1d',start="2010-01-01")
+data_load_state.text('Loading data... done!')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
+
+# preparing the data for Facebook-Prophet.
+data = data.reset_index()
+data_pred = data[['Date','Close']]
+data_pred=data_pred.rename(columns={"Date": "ds", "Close": "y"})
